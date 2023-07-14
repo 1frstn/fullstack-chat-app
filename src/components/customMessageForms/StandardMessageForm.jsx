b31f15ffd2c92d80b9d3a656c4b7a70b26f928f7
1frstn <krmilhn@icloud.com>
2023-07-14 20:45:34 +1000

    server sideAdded

--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -2,13 +2,34 @@ import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from "@heroicons/react/24
 import { useState } from "react"
 import Dropzone from "react-dropzone";
 
-const StandardMessageForm = () => {
+const StandardMessageForm = ({props, activeChat}) => {
+  console.log("activeChat:", activeChat)
+  console.log("props:", props)
   
   const [message, setMessage] = useState("");
   const [attachment, setAttachmnet] = useState("");
   const [preview, setPreview] = useState("");
 
   const handleChange = (e) => setMessage(e.    target.value)
+
+  const handleSubmit = async () => {
+    const date = new Date().toISOString()
+          .replace("T"," ")
+          .replace("Z",`${Math.floor(Math.random()*1000)}+00:00`)
+    console.log("ATTACHMETN",attachment);      
+    const at = attachment ? [{ blob: attachment, file: attachment.name}] : [];
+    const form = {
+      attachments: at,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    }
+
+    props.onSubmit(form);
+    setMessage("");
+    setAttachmnet("")
+  } 
 
   return (
     <div className='message-form-container' >
@@ -63,13 +84,13 @@ const StandardMessageForm = () => {
                 className="message-form-icon-airplane"
                 onClick={() => {
                   setPreview("");
-                  /* handleSubmit(); */
+                  handleSubmit();
                 }} />
         </div>
       </div>
     </div>
     
   )
-}
+};
 
 export default StandardMessageForm;
